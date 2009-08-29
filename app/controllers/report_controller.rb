4740f2dfb36161d27bf74ecab1e80c4a04ba650c
Erik DeBill <edebill@erik-debills-macbook-pro.local>
2009-08-29 13:16:30 -0500

    can graph multiple sources against each other.  will go "boom" if one has no data during the period requested.

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -35,72 +35,31 @@ class ReportController < ApplicationController
       format.json {
         title = Title.new("Recent Temperatures")
 
-        data1 = []
 
-        report_end = @report.end || Time.now.utc
-        temps = FahrenheitTemp.find(:all,
-                                    :conditions => [ 'source_id = ? and sampled_at > ? and sampled_at < ?', @source.id,  @report.start, report_end],
-                                    :order => :sampled_at)
+        temps = ReportController.prepare_temps(@report)
 
-        step_size = ReportController.calculate_step_size(temps)
-        first_time = ReportController.calculate_first_time(temps[0].sampled_at, step_size)
+        (first_reading, last_reading) = ReportController.find_first_and_last_reading(temps)
+        step_size = ReportController.calculate_step_size(first_reading, last_reading)
+        (first_time, last_time) = ReportController.calculate_first_and_last_time(first_reading, last_reading, step_size)
+        # kind of subtle - start at calculated first time, but needs
+        # actual last reading
+        x = x_axis(first_time, last_reading, step_size)
         
-        by_temp = temps.sort { |a,b| a.temp <=> b.temp }
 
-        max = Integer(by_temp[-1].temp / 5.0) * 5 + 5
-        min = Integer(by_temp[0].temp / 5.0) * 5
 
-        x_labels = XAxisLabels.new
 
-        labels = []
-        seconds_diff = temps[-1].sampled_at - temps[0].sampled_at
-        steps = Integer((seconds_diff / 60.0) / step_size) * step_size + step_size
+        (highest_reading, lowest_reading) = ReportController.find_highest_and_lowest_readings(temps)
 
-        step_time = first_time
-        step_no = 0
-        while step_time < temps[-1].sampled_at
-          next_step = step_time + 60 * step_size
+        max = Integer(highest_reading / 5.0) * 5 + 5
+        min = Integer(lowest_reading / 5.0) * 5
 
-          display_time = step_time.strftime("%b %d, %k:%M %Z")
-          
-          if step_no % 5 == 0
-            labels <<  XAxisLabel.new(display_time, '#0000ff', 20, 80)
-          else 
-            labels << nil
-          end
-          
-          t_total = 0.0
-          t_count = 0.0
-          temps.each do |t|
-            if t.sampled_at > step_time &&
-                t.sampled_at <=  next_step
-              t_total +=  t.temp
-              t_count += 1
-            end
-          end
+        
+        temps.each do |source|
+          t = source[:readings]
 
-          if(t_count > 0)
-            data1 << t_total / t_count
-          else
-            data1 << nil
-          end
-
-          step_time = next_step
-          step_no += 1
+          source[:data] = calculate_graph_data_for_source(t, first_time, last_reading, step_size)
         end
 
-        x_labels.labels = labels
-
-        x = XAxis.new
-        x.set_labels(x_labels)
-
-
-        line_dot = LineDot.new
-        line_dot.text = "Line Dot"
-        line_dot.width = 1
-        line_dot.colour = '#5E4725'
-        line_dot.dot_size = 2
-        line_dot.values = data1
 
         y = YAxis.new
         y.set_range(min,max,5)
@@ -119,7 +78,16 @@ class ReportController < ApplicationController
         chart.y_axis = y
         chart.x_axis = x
 
-        chart.add_element(line_dot)
+        colors = [ '#000000', '#FF0000', '#00FF00',  '#0000FF']
+        temps.each do |source|
+          line_dot = LineDot.new
+          line_dot.text = source[:source].name
+          line_dot.width = 1
+          line_dot.colour = colors.shift || '#999999'
+          line_dot.dot_size = 2
+          line_dot.values = source[:data]
+          chart.add_element(line_dot)
+        end
 
         render :text => chart.to_s
       }
@@ -129,8 +97,29 @@ class ReportController < ApplicationController
 
   protected
 
-  def self.calculate_step_size(temps)
-    secs = temps[-1].sampled_at - temps[0].sampled_at
+  def self.find_first_and_last_reading(temps)
+    first = nil
+    last = nil
+    temps.each do |source|
+      t = source[:readings]
+
+      if t[0].blank?
+        next
+      end
+      if  first == nil || t[0].sampled_at < first
+        first = t[0].sampled_at
+      end
+
+      if last == nil || t[-1].sampled_at > last
+        last = t[-1].sampled_at
+      end
+    end
+    return first, last
+  end
+
+  def self.calculate_step_size(first, last)
+
+    secs = last - first
 
     minutes = secs/60
 
@@ -143,7 +132,106 @@ class ReportController < ApplicationController
     return step
   end
 
-  def self.calculate_first_time(earliest, step_size)
-    return Time.at(Integer((earliest.to_i / 60.0) / step_size) * step_size * 60)
+  def self.calculate_first_and_last_time(first, last, step_size)
+
+    return Time.at(Integer((first.to_i / 60.0) / step_size) * step_size * 60), 
+           Time.at((((last.to_i / 60.0).to_i / step_size) + 1) * step_size * 60)
   end
+
+  def x_axis(first_time, last_time, step_size)
+    x_labels = XAxisLabels.new
+    labels = []
+
+    step_through_times(first_time, last_time, step_size) do |this_step, next_step, step_no| 
+
+      display_time = this_step.strftime("%b %d, %k:%M %Z")
+      
+      if step_no % 5 == 0
+        labels <<  XAxisLabel.new(display_time, '#0000ff', 20, 80)
+      else 
+        labels << nil
+      end
+    end
+
+    x_labels.labels = labels
+
+    x = XAxis.new
+    x.set_labels(x_labels)
+
+    return x
+  end
+
+  def self.find_highest_and_lowest_readings(temps)
+    logger.debug("find highest and lowest readings")
+    highest = nil
+    lowest = nil
+    temps.each do |source|
+      logger.debug("checking highest and lowest for #{source[:source].name}")
+      t = source[:readings]
+      by_temp = t.sort { |x, y| x.temp <=> y.temp }
+      logger.debug("#{by_temp[0].temp} - #{by_temp[-1].temp}")
+      if lowest == nil || by_temp[0].temp < lowest
+        lowest = by_temp[0].temp
+      end
+      if highest == nil || by_temp[-1].temp > highest
+        highest = by_temp[-1].temp
+      end
+    end
+
+    return highest, lowest
+  end
+  
+  def calculate_graph_data_for_source(temps, first_time, last_reading, step_size)
+    data = []
+    total = 0.0
+    count = nil
+
+    step_through_times(first_time, last_reading, step_size) do |this_step, next_step, step_no|
+      temps.each do |t|
+        if t.sampled_at > this_step && t.sampled_at <= next_step
+          total += t.temp
+          count = count.to_i + 1  # in case it is nil
+        end
+      end
+      
+      if(count.nil?)
+        data << nil
+      else
+        data << total / count
+      end
+    end
+
+    return data
+  end
+
+  def step_through_times(first_time, last_time, step_size)
+    this_step = first_time
+    step_no = 0
+    while this_step < last_time
+      next_step = this_step + 60 * step_size
+
+      yield(this_step, next_step, step_no)
+
+      this_step = next_step
+      step_no += 1
+    end
+  end
+
+
+  def self.prepare_temps(report)
+
+    report_end = report.end || Time.now.utc
+    
+    temps = []
+    report.sources.each do |source|
+      temps << { :source => source,
+        :readings =>  FahrenheitTemp.find(:all,
+                                          :conditions => [ 'source_id = ? and sampled_at > ? and sampled_at < ?', source.id,  report.start, report_end],
+                                          :order => :sampled_at) || [],
+        :data => []
+      }
+    end
+    return temps
+  end
+
 end
