afd82c333702624f7b9efe78759429d1a3b3f8ad
Erik DeBill <erik@debill.org>
2010-02-13 09:57:11 -0600

    switch to more modern serialport module, from ruby-serialport

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "ruby-serialport", :lib => "serialport"
+  config.gem "serialport"
   config.gem 'shoulda'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
